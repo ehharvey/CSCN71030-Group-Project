50781ee738eed9582553ffc8c03403b43c9baf69
Emil Harvey <36806437+ehharvey@users.noreply.github.com>
2021-03-23 13:10:12 -0400

    Updated Object Interface

--- CSCN71030-Group-Project/CSCN71030-Group-Project/Object.h
+++ CSCN71030-Group-Project/CSCN71030-Group-Project/Object.h
@@ -1,13 +1,27 @@
 #pragma once
 #include <vector>
 
+class ItemSlot {
+	void* item; // TODO: change to &item
+	int slot;
+public:
+	ItemSlot(void* item, int slot); // TODO: Change Item*
+	~ItemSlot();
+
+	int getSlot();
+	void* getItem();
+};
+
 class Inventory {
+	int _next_slot;
 	int size;
-	std::vector<void*> container; // TODO: Change to Item*
+	std::vector<ItemSlot&> container;
 public:
 	Inventory();
 	~Inventory();
 
 	void insertItem(void* item);
+	void removeItem(ItemSlot& item_slot);
+
 	std::vector<void*> getContainer(); // Returns copy of container
 };
