9b4a7f0d2bb992d853e255bd0ad9bea96e0c1305
ehharvey <36806437+ehharvey@users.noreply.github.com>
2021-03-25 10:36:36 -0400

    Imported interfaces from Emil's modules

--- CSCN71030-Group-Project/CSCN71030-Group-Project/Stubs.h
+++ CSCN71030-Group-Project/CSCN71030-Group-Project/Stubs.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <vector>
 
 // Character
 class Character {
@@ -19,3 +20,78 @@ public:
 
 	explicit operator std::string();
 };
+
+// ReadWrite
+// NOTE: A filesystem module is available, but only for c++17
+
+typedef void* GAME_STATE; // Placeholder
+
+class LoadEntry {
+	std::string name;
+	std::string full_path;
+public:
+	LoadEntry(std::string name, std::string full_path);
+	~LoadEntry();
+
+	std::string getName();
+	GAME_STATE loadEntry();
+};
+
+
+class Loader {
+	std::string root;
+	std::vector<LoadEntry&> load_entries;
+public:
+	Loader(std::string root);
+	~Loader();
+
+	std::vector<LoadEntry&> get_entries();
+};
+
+class Save {
+	std::string root;
+	std::string name;
+	bool _name_is_unique;
+	bool _was_successful;
+public:
+	Save(std::string root, std::string name);
+
+	bool name_is_unique();
+	bool was_successful();
+};
+
+
+
+// Object 
+// Note: We are planning to remove Objects from the product!
+
+
+typedef void* ITEM; // Placeholder
+
+
+class ItemSlot {
+	ITEM item; // TODO: change to &item
+	int slot;
+public:
+	ItemSlot(ITEM item, int slot); // TODO: Change Item*
+	~ItemSlot();
+
+	int getSlot();
+	void* getItem();
+};
+
+class Inventory {
+	int _next_slot;
+	int size;
+	std::vector<ItemSlot&> container;
+public:
+	Inventory();
+	~Inventory();
+
+	void insertItem(ITEM item);
+	void removeItem(ItemSlot& item_slot);
+
+	std::vector<void*> getContainer(); // Returns copy of container
+};
+
+
