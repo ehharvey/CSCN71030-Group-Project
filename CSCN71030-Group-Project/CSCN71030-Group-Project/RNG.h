cbd617f9ea2642b5fc75bd2c0f180447ac413569
ehharvey <36806437+ehharvey@users.noreply.github.com>
2021-03-27 07:58:10 -0400

    RNG interface

---
+++ CSCN71030-Group-Project/CSCN71030-Group-Project/RNG.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+struct CharacterValues {
+    int value_one;
+    int value_two;
+    int value_three;
+    int value_four;
+
+    CharacterValues();
+};
+
+int getEncounterIndex();
+
+int getEnemyIndex();
