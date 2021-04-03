c6c20c570165d21916032b870cc3756b8187f6aa
colefoster <71045117+colefoster@users.noreply.github.com>
2021-04-03 10:18:19 -0400

    Level Module merge to main (#17)
    * Initial character interface
    
    * Imported interfaces from Emil's modules
    
    * Updates to Character interface. Other small changes
    
    * Added additional members and functions to the character interface, and a stub for UI (it's still may need revisions but that will be done in future commits)
    
    * Added additional features to character class
    
    * added level.h and level.cpp
    
    * Added to combat loop
    
    * Flushed out combat further
    
    * small level adjustments
    
    Co-authored-by: ehharvey <36806437+ehharvey@users.noreply.github.com>
    Co-authored-by: Rukia Al-Amiri <Ralamiri4608@conestogac.on.ca>

---
+++ CSCN71030-Group-Project/CSCN71030-Group-Project/Level.cpp
@@ -0,0 +1,109 @@
+#include "Level.h"
+#include "Stubs.h"
+
+int userInput;
+Level::Level() {
+
+}
+Level::Level(Character* player, Character* enemy, stageType type ) {
+	this->playerCharacter = player;
+	this->enemyCharacter = enemy;
+	this->type = type;
+}
+Character* Level::getPlayer() {
+	return this->playerCharacter;
+}
+Character* Level::getEnemy() {
+	return this->enemyCharacter;
+}
+stageType Level::getStageType() {
+	return this->type;
+}
+
+int Level::enterCombat(Level* levelInfo) {
+	int userInput;
+	int turn = 0;
+	while (!levelInfo->getPlayer()->getHealth() && !levelInfo->getEnemy()->getHealth()) {
+
+		
+		userInput = someUIObject.get_input();
+
+		switch (userInput)
+		{
+		case Attack:
+			//if (levelInfo->getPlayer()->isPrepared())
+			{
+				if (simEnemyCombat(turn) == Prepare) {
+					//Player attacks while enemy preps
+					calculateDamage(levelInfo->getPlayer(), levelInfo->getEnemy());
+				}
+				else if (simEnemyCombat(turn) == Dodge) {
+					//enemy dodges player's attack
+				}
+				else if (simEnemyCombat(turn) == Attack) {
+					//both players attack
+					calculateDamage(levelInfo->getPlayer(), levelInfo->getEnemy());
+					calculateDamage(levelInfo->getEnemy(), levelInfo->getPlayer());
+				}
+			}
+			//else
+			{
+				//ui
+			}
+			break;
+		case Dodge:
+			if (simEnemyCombat(turn) == Prepare) {
+				//player dodges, enemy preps
+			}
+			else if (simEnemyCombat(turn) == Dodge) {
+				//both players dodge
+			}
+			else if (simEnemyCombat(turn) == Attack) {
+				//enemy attacks, player dodges
+			}
+			break;
+		case Prepare:
+			//levelInfo->getPlayer()->isPrepared();
+			if (simEnemyCombat(turn) == Prepare) {
+				//both players prep
+				//levelInfo->getEnemy()->isPrepared();
+			}
+			else if (simEnemyCombat(turn) == Dodge) {
+				//player preps, enemy dodges
+			}
+			else if (simEnemyCombat(turn) == Attack) {
+				//player preps, enemy attacks
+				calculateDamage(levelInfo->getEnemy(), levelInfo->getPlayer());
+			}
+			break;
+		default:
+			break;
+		}
+		turn++;
+
+	}
+}
+
+combatOptions Level::simEnemyCombat(int turn) {
+	switch (turn%3)	//linear combat pattern
+	{
+	case 0:
+		return Prepare;//turn 1 prep
+		break;
+	case 1:
+		return Dodge;//turn 2 defend
+		break;
+	case 2:
+		return Attack;//turn 3 attack
+		break;
+	default:
+		break;
+	}
+}
+void calculateDamage(Character* attacker, Character* defender) {
+	defender->setHealth(defender->getHealth() - attacker->getAttack()); //subtracts attack from health
+	return;
+}
+Level::~Level() {
+
+}
