97e2bfd46c13e8bddcc14389921a00eba6bbc843
Rukia Al-Amiri <Ralamiri4608@conestogac.on.ca>
2021-04-07 10:37:27 -0400

    Added Character Test cases

--- CSCN71030-Group-Project/CSCN71030-Group-Project/Stubs.h
+++ CSCN71030-Group-Project/CSCN71030-Group-Project/Stubs.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <string>
 #include <vector>
-
+#include <nlohmann/json.hpp>
 // The placeholders (need to define even in stubs eventually!)
 typedef void* GAME_STATE; // Placeholder
 typedef void* ITEM; // Placeholder
@@ -9,37 +9,276 @@ typedef void* ITEM; // Placeholder
 
 // Character ------------------------------------------------------------------
 
+using json = nlohmann::json;
+
 class Character {
 	std::string name;
-	
 	int current_health;
-
 	int max_health;
-	
 	int attack;
-
-	int defense;
-
+	int defence;
 	int speed;
+	Character* Character_PTR;
+	std::string sprite;
+	std::string fight_sprite;
+	std::string dead_sprite;
+	bool isPrepared;
 
-	enum character_class { spoon, fork, knife };
+public:
+
+	Character();											// Default constructor
+
+	Character(std::string name);							// New character
+
+	Character(json j);
+
+	virtual json jsonify();
+
+	virtual void setHealth(int health_reduction);
+
+	virtual int getHealth();
+
+	virtual int getAttack();
+
+	virtual int getDefence();
+
+	virtual int getSpeed();
+
+	virtual std::string getSprite();
+
+	virtual std::string getFightSprite();
+
+	virtual std::string getDeadSprite();
+
+	virtual std::string getName();
+
+	virtual bool getPrepared();
+
+	virtual bool setPrepared(bool);
+
+};
+
+
+class spoon : public Character {
+
+	std::string name;
+	int current_health;
+	int max_health = 10;
+	int attack = 5;
+	int defence = 10;
+	int speed = 4;
+	std::string sprite;
+	std::string fight_sprite;
+	std::string dead_sprite;
 
 	Character* Character_PTR;
-	
+	bool isPrepared;
+
 public:
-	Character(std::string name, std::string character_class); // New character
-	
-	// Cpp has a built-in regex library; can use to represent
-	//   Characters as a string (e.g., delimited string)
-	Character(std::string restore); // Restore from saving
+	spoon();												// Default constructor
+	spoon(std::string name);								// New spoon character
+	json jsonify();
+};
 
-	void setHealth(int health);
+class fork : public Character {
 
-	int getHealth();
-	
-	int getAttack();
+	std::string name;
+	int current_health;
+	int max_health = 7;
+	int attack = 7;
+	int defence = 5;
+	int speed = 10;
+	std::string sprite;
+	std::string fight_sprite;
+	std::string dead_sprite;
 
-	explicit operator std::string(); // To typecast a character to a string
+	Character* Character_PTR;
+	bool isPrepared;
+
+public:
+	fork();												// Default constructor
+	fork(std::string name);								// New fork character
+	json jsonify();
+};
+
+
+class knife : public Character {
+
+	std::string name;
+	int current_health;
+	int max_health = 5;
+	int attack = 10;
+	int defence = 2;
+	int speed = 8;
+	std::string sprite;
+	std::string fight_sprite;
+	std::string dead_sprite;
+
+	Character* Character_PTR;
+	bool isPrepared;
+
+public:
+	knife();												// Default constructor
+	knife(std::string name);								// New knife character
+	json jsonify();
+};
+
+class DrawerEnemy : public Character {
+
+	std::string name;
+	int current_health;
+	int max_health;
+	int attack;
+	int defence;
+	int speed;
+	std::string sprite;
+	std::string dead_sprite;
+
+	Character* Enemy_PTR;
+	bool isPrepared;
+
+public:
+	DrawerEnemy(int, int, int, int);												// Constructor
+};
+
+class DrawerBoss : public Character {
+
+	std::string name;
+	int current_health;
+	int max_health;
+	int attack;
+	int defence;
+	int speed;
+	std::string sprite;
+	std::string dead_sprite;
+
+	Character* Enemy_PTR;
+	bool isPrepared;
+
+public:
+	DrawerBoss(int, int, int, int);												// Constructor
+};
+
+class SinkEnemy : public Character {
+
+	std::string name;
+	int current_health;
+	int max_health;
+	int attack;
+	int defence;
+	int speed;
+	std::string sprite;
+	std::string dead_sprite;
+
+	Character* Enemy_PTR;
+	bool isPrepared;
+
+public:
+	SinkEnemy(int, int, int, int);												// Constructor
+};
+
+class SinkBoss : public Character {
+	std::string name;
+	int current_health;
+	int max_health;
+	int attack;
+	int defence;
+	int speed;
+	std::string sprite;
+	std::string dead_sprite;
+
+	Character* Enemy_PTR;
+	bool isPrepared;
+
+public:
+	SinkBoss(int, int, int, int);												// Constructor
+};
+
+class OvenEnemy : public Character {
+
+	std::string name;
+	int current_health;
+	int max_health;
+	int attack;
+	int defence;
+	int speed;
+	std::string sprite;
+	std::string dead_sprite;
+
+	Character* Enemy_PTR;
+	bool isPrepared;
+
+public:
+	OvenEnemy(int, int, int, int);												// Constructor
+};
+class OvenBoss : public Character {
+
+	std::string name;
+	int current_health;
+	int max_health;
+	int attack;
+	int defence;
+	int speed;
+	std::string sprite;
+	std::string dead_sprite;
+
+	Character* Enemy_PTR;
+	bool isPrepared;
+
+public:
+	OvenBoss(int, int, int, int);												// Constructor
+};
+class CounterEnemy : public Character {
+
+	std::string name;
+	int current_health;
+	int max_health;
+	int attack;
+	int defence;
+	int speed;
+	std::string sprite;
+	std::string dead_sprite;
+
+	Character* Enemy_PTR;
+	bool isPrepared;
+
+public:
+	CounterEnemy(int, int, int, int);												// Constructor
+};
+class CounterBoss : public Character {
+
+	std::string name;
+	int current_health;
+	int max_health;
+	int attack;
+	int defence;
+	int speed;
+	std::string sprite;
+	std::string dead_sprite;
+
+	Character* Enemy_PTR;
+	bool isPrepared;
+
+public:
+	CounterBoss(int, int, int, int);												// Constructor
+};
+
+class FinalBoss : public Character {
+
+	std::string name;
+	int current_health;
+	int max_health;
+	int attack;
+	int defence;
+	int speed;
+	std::string sprite;
+	std::string dead_sprite;
+
+	Character* Enemy_PTR;
+	bool isPrepared;
+
+public:
+	FinalBoss(int, int, int, int);												// Constructor
 };
 // ----------------------------------------------------------------------------
 
