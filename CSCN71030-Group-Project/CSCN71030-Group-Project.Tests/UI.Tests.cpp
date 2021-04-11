a18adf9e8dcfb54fdafc53c63a376c7059cbc41e
Rukia Al-Amiri <Ralamiri4608@conestogac.on.ca>
2021-04-11 16:03:38 -0400

    Added UI Testing files and Test Names

---
+++ CSCN71030-Group-Project/CSCN71030-Group-Project.Tests/UI.Tests.cpp
@@ -0,0 +1,165 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../CSCN71030-Group-Project/Character.h"
+#include"../CSCN71030-Group-Project/UI.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UITests
+{
+	TEST_CLASS(UIIntegrationTests)
+	{
+	public:
+
+		TEST_METHOD(UIIntegrationTests001DisplayEnounterOptionsDuringEncounter_StartEncounter_OptionsToConsole)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests002GetUserInput_UserChoice_ChoiceBasedAction)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests003OutputSpritesToConsoleForEncounter_StartEnounter_SpritesPrintToConsole)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests004CleanExitAfterLoad_ExitCodeInput_ExitGame)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests005CleanExitDuringEncounter_ExitCodeInput_ExitGame)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests006GetSpoonSprite_Spoon_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests007GetForkSprite_Fork_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests008GetKnifeSprite_Knife_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests009GetSpoonFightSprite_Spoon_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests010GetForkFightSprite_Fork_ConsoleOutput)
+		{
+
+		}
+
+		TEST_METHOD(UIIntegrationTests011GetKnifeFightSprite_Knife_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests012GetSpoonDeadSprite_Spoon_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests013GetForkDeadSprite_Fork_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests014GetKnifeDeadSprite_Knife_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests015GetDrawerEnemySprite_DrawerEnemy_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests016GetDrawerBossSprite_DrawerBoss_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests017GetSinkEnemySprite_SinkEnemy_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests018GetSinkBossSprite_SinkBoss_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests019GetOvenEnemySprite_OvenEnemy_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests020GetOvenBossSprite_OvenBoss_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests021GetCounterEnemySprite_CounterEnemy_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests022GetCounterBossSprite_CounterBoss_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests023GetFinalBossSprite_FinalBoss_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests024GetDrawerEnemyDeadSprite_DrawerEnemy_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests025GetDrawerBossDeadSprite_DrawerBoss_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests026GetSinkEnemyDeadSprite_SinkEnemy_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests027GetSinkBossDeadSprite_SinkBoss_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests028GetOvenEnemyDeadSprite_OvenEnemy_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests029GetOvenBossDeadSprite_OvenBoss_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests030GetCounterEnemyDeadSprite_CounterEnemy_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests031GetCounterBossDeadSprite_CounterBoss_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests032GetFinalBossDeadSprite_FinalBoss_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests033GameOver_UserCharacter_ConsoleOutput)
+		{
+
+		}
+		TEST_METHOD(UIIntegrationTests034GameWon_UserCharacter_ConsoleOutput)
+		{
+
+		}
+
+	};
+
+
+	TEST_CLASS(UITests)
+	{
+	public:
+
+		TEST_METHOD(UITests001CleanExit_ExitCodeInput_ExitGame)
+		{
+
+		}
+	};
+}
