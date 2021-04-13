886b654189486726c2e4d1ffb9fa9cd7c76fdd12
ehharvey <36806437+ehharvey@users.noreply.github.com>
2021-04-13 01:03:11 -0400

    Revert "Rough code for Level completed! Some pseudo  UI"
    This reverts commit 8ba039d43694f2e473ac526c49b14bc7f60e4870.

--- CSCN71030-Group-Project/CSCN71030-Group-Project/Level.h
+++ CSCN71030-Group-Project/CSCN71030-Group-Project/Level.h
@@ -3,23 +3,19 @@
 
 #pragma once
 #include "Character.h"
-#include "UI.h"
 
 enum stageType { Drawer = 0, Sink = 1, Oven = 2, Counter = 3 };
 enum combatOptions {Attack, Dodge, Prepare};
-enum combatStatus { Win, Die, InProgress };
-
 class Level {
 	Character* playerCharacter;	//references to both player and enemy characters
 	Character* enemyCharacter;
 	stageType type;
 
+
 public:
 	Level();
 	~Level();
 	Level(Character* player, Character* enemy, stageType type);
-
-	Level(Character* player, stageType type);
 	
 	Character* getPlayer();
 	Character* getEnemy();
@@ -27,10 +23,6 @@ public:
 	int simEnemyCombat(int turn);
 	void calculateDamage(Character* attacker, Character* defender);
 	int enterCombat(Level* currentLevel);
-
-	input_choice getEnemyChoice(int turn);
-	combatStatus enterCombat(void);
-	combatStatus combatShouldContinue();
 };
 
 #endif // !LEVEL_H
